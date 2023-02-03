a27d6c6b6950dc1de65eda5442b53a4ce878a022
Jura19777 <poryadin77@gmail.com>
2023-02-03 19:44:40 +0700

    Задать двумерный массив следующим правилом: Aₘₙ = m+n - ответ

---
+++ Ext_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int ReadInt(string str)
+{
+    Console.Write(str);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] CreateMTR(int m, int n)
+{
+    int[,] mtr = new int [m,n];
+    for (int i=0; i<mtr.GetLength(0); i++)
+        for (int j=0; j<mtr.GetLength(1); j++)
+            mtr[i,j] = i+j;
+        return mtr;
+}
+
+void PrintMTR(int[,] mtr)
+{
+    for (int i=0; i<mtr.GetLength(0); i++, Console.WriteLine())
+        for (int j=0; j<mtr.GetLength(1); j++)
+            Console.Write($"{mtr[i,j]} ");
+}
+
+int m = ReadInt("ВВедите m: ");
+int n = ReadInt("ВВедите n: ");
+int[,] mtr = CreateMTR(m,n);
+Console.WriteLine("Выводим матрицу");
+PrintMTR(mtr);
